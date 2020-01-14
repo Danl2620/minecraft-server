0cc6bd6312aa25e7ac5b04baa9aee36956195a4c
Dan Liebgold <dan.liebgold@gmail.com>
2020-01-13 18:13:38 -0800

    access

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/lib/jvm/jre-1.8.0-openjdk/bin/java -Xms512M -Xmx1024M -jar minecraft_server.1.15.1.jar 
+screen -S minecraft-server -d -m /usr/lib/jvm/jre-1.8.0-openjdk/bin/java -Xms512M -Xmx1024M -jar minecraft_server.1.15.1.jar 
