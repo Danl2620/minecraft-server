2ce26c0a6f18a8fc3ef302dffaa5ddbdb7f5157b
BuildTools <unconfigured@null.spigotmc.org>
2020-06-04 15:14:22 +0000

    claims, playerdata, config

--- etc/launch-spigot.sh
+++ etc/launch-spigot.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-screen -S minecraft-server -d -m /usr/lib/jvm/jre-1.8.0-openjdk/bin/java -Xms512M -Xmx1024M -jar ./Spigot/Spigot-Server/target/spigot-1.15.1-R0.1-SNAPSHOT.jar
+VER=1.15.2
+screen -S minecraft-server -d -m /usr/lib/jvm/jre-1.8.0-openjdk/bin/java -Xms512M -Xmx1024M -jar ./Spigot/Spigot-Server/target/spigot-${VER}-R0.1-SNAPSHOT.jar
